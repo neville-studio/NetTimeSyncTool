7bfac5e391d0bac690893a4002e147c7a3070670
王徐鹏 <2840772674@qq.com>
2023-12-07 00:05:15 +0800

    init version

--- NetTimeSyncTool/resource.h
+++ NetTimeSyncTool/resource.h
@@ -34,6 +34,7 @@
 #define IDR_MAINFRAME                   128
 #define IDD_ADDDIALOG                   132
 #define IDD_ADDDIALOG1                  133
+#define IDR_VERSION2                    133
 #define IDC_EDIT1                       1000
 #define IDC_SERVER                      1001
 #define ID_ACCELERATOR32772             32772
@@ -44,7 +45,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32773
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           110
