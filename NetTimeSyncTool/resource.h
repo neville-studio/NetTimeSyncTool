79db3f02872eeebe50b569d162de96f8a5382111
王徐鹏 <2840772674@qq.com>
2024-05-17 14:41:27 +0800

    Updated Resources, which makes the it stable.

--- NetTimeSyncTool/resource.h
+++ NetTimeSyncTool/resource.h
@@ -48,7 +48,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        134
-#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_COMMAND_VALUE         32774
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
